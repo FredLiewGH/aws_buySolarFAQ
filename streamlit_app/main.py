2beee58b8c5d3f3418c9cb60ab9b6a7fa3ae828e
Meowbot <114375294+FredLiewGH@users.noreply.github.com>
2024-07-09 15:33:42 +0800

    Update main.py

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -1,9 +1,10 @@
-from Logging import CustomLogger
+#from Logging import CustomLogger
 from Converse import startChat
-from datetime import datetime
+#from datetime import datetime
 
 # Main script 
 if __name__ == '__main__':
-    logdate = str(datetime.today().strftime('%Y%m%d'))
-    logger = CustomLogger("buySolarFAQAgent/streamlit_app/Logs", f"{logdate}_buySolarFAQAgent.txt") 
-    startChat(logger)
+    #logdate = str(datetime.today().strftime('%Y%m%d'))
+    #logger = CustomLogger("buySolarFAQAgent/streamlit_app/Logs", f"{logdate}_buySolarFAQAgent.txt") 
+    #startChat(logger)
+    startChat()
