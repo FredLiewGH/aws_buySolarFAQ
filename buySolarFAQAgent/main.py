1a1621948100d5a328930006549e136091111559
Meowbot <114375294+FredLiewGH@users.noreply.github.com>
2024-07-09 15:29:14 +0800

    Delete buySolarFAQAgent directory

--- buySolarFAQAgent/main.py
+++
@@ -1,9 +0,0 @@
-from InvokeAgent.Common.Logging import CustomLogger
-from InvokeAgent.Modules.Converse import startChat
-from datetime import datetime
-
-# Main script 
-if __name__ == '__main__':
-    logdate = str(datetime.today().strftime('%Y%m%d'))
-    logger = CustomLogger(f"D:\\Source\\Python\\Logs", f"{logdate}_buySolarFAQAgent.txt") 
-    startChat(logger)
