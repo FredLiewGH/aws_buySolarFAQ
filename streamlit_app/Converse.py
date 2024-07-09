94b686ea01090eea9019e5776f4c5f728249ee4f
Meowbot <114375294+FredLiewGH@users.noreply.github.com>
2024-07-09 15:34:00 +0800

    Update Converse.py

--- streamlit_app/Converse.py
+++ streamlit_app/Converse.py
@@ -5,56 +5,57 @@ import json
 import random
 
 # Write conversation to file
-def writeToFile(filedir, filename, content, logger):
-    try:
-        if not os.path.exists(filedir): 
-            os.makedirs(filedir) 
+# def writeToFile(filedir, filename, content, logger):
+#     try:
+#         if not os.path.exists(filedir): 
+#             os.makedirs(filedir) 
     
-        filepath = os.path.join(filedir, filename)    
-        with open(filepath, "a") as myfile:        
-            myfile.write(f"{content}\n\n")
+#         filepath = os.path.join(filedir, filename)    
+#         with open(filepath, "a") as myfile:        
+#             myfile.write(f"{content}\n\n")
 
-        logger.info(f"Write to file: {filepath}")
-    except Exception as e:
-        logger.exception(f"Error write to file: {e}", exc_info=True)
-
-
-def getThankResponse(logger):
-    try:
-        filepath = "buySolarFAQAgent/streamlit_app/General_Responses_Thank_v1.0.txt"
-        with open(filepath, "r") as myfile:        
-            lines = myfile.readlines()
-
-            if lines.__len__ == 0:
-                return "You're welcome! Is there anything else I can help you with?"
-            else:
-                return (random.choice(lines))
-    except Exception as e:
-        logger.exception(f"Error read from response file: {e}", exc_info=True) 
+#         logger.info(f"Write to file: {filepath}")
+#     except Exception as e:
+#         logger.exception(f"Error write to file: {e}", exc_info=True)
 
 
-def startChat(logger):
+# def getThankResponse(logger):
+#     try:
+#         filepath = "buySolarFAQAgent/streamlit_app/General_Responses_Thank_v1.0.txt"
+#         with open(filepath, "r") as myfile:        
+#             lines = myfile.readlines()
+
+#             if lines.__len__ == 0:
+#                 return "You're welcome! Is there anything else I can help you with?"
+#             else:
+#                 return (random.choice(lines))
+#     except Exception as e:
+#         logger.exception(f"Error read from response file: {e}", exc_info=True) 
+
+
+def startChat():
     url = "https://5gldi32bl5.execute-api.us-east-1.amazonaws.com/buySolar/func_buySolar_FAQ"    
 
     # Write to chat history file
     session_id = uuid.uuid4().hex    
-    file_chatDir = "buySolarFAQAgent/streamlit_app/ChatHistory"
-    file_chatHist = f"{session_id}.txt"
-    writeToFile(file_chatDir, file_chatHist, f"Start Chat Session ID: {session_id}", logger)
+    # file_chatDir = "buySolarFAQAgent/streamlit_app/ChatHistory"
+    # file_chatHist = f"{session_id}.txt"
+    # writeToFile(file_chatDir, file_chatHist, f"Start Chat Session ID: {session_id}", logger)
 
     greetMsg = "Bot: Hello! Welcome to buySolar. How may I help you?"
     print(f"{greetMsg}\n")
-    writeToFile(file_chatDir, file_chatHist, greetMsg, logger)
+    # writeToFile(file_chatDir, file_chatHist, greetMsg, logger)
     
     while True:
         prompt = input("Human: ")
-        writeToFile(file_chatDir, file_chatHist, F"Human: {prompt}", logger)
+        # writeToFile(file_chatDir, file_chatHist, F"Human: {prompt}", logger)
 
         ### Hardcoded response due to prompt script unable to return the correct response to thank
         if "thank" in prompt.lower():
-            thankMsg = getThankResponse(logger)
+            # thankMsg = getThankResponse(logger)
+            thankMsg = "No problem at all! Thank you for choosing buySolar. We value your business and are committed to providing excellent customer service. Is there anything else I can help with today?"
             print(f"\n{thankMsg}\n")
-            writeToFile(file_chatDir, file_chatHist, thankMsg, logger)
+            # writeToFile(file_chatDir, file_chatHist, thankMsg, logger)
         else:
             payload = json.dumps({
                 "prompt": prompt,
@@ -68,4 +69,4 @@ def startChat(logger):
 
             respondMsg = f"Bot: {response.text}"
             print(f"\n{respondMsg}\n")
-            writeToFile(file_chatDir, file_chatHist, respondMsg, logger)
+            # writeToFile(file_chatDir, file_chatHist, respondMsg, logger)
